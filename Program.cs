dc56dbabd7a2d7882bd95240fd64e6eaebbcfe6d
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-27 18:50:15 +0300

    Открываем файл Program.cs и прописываем подключение к базе данных

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using DMassage.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+// Подключение к локальной базе данных SQLite
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite("Data Source=DMassage.db"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapDefaultControllerRoute();
 
 app.Run();
