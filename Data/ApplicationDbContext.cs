073d92d2fad59a791737b9565eb0384dda8e3121
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-27 18:49:05 +0300

    Создаем класс контекста базы данных в папке Data, чтобы определить связь между нашими моделями и таблицами в базе данных.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DMassage.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DMassage.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Massage> Massages { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
