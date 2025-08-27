5dc9d4b1a956d1666c6742cce5ee49f9b3619626
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-27 18:48:01 +0300

    Создал сущности Массажа и Брони

---
+++ Models/Massage.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DMassage.Models
+{
+    public class Massage
+    {
+        public int Id { get; set; }
+
+        // Название услуги
+        [Required]
+        public string Name { get; set; }
+
+        // Описание услуги
+        [Required]
+        public string Description { get; set; }
+
+        // Цена услуги
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+    }
+}
